55617d0406e2064ad3bb4f0898e3591eb0376bc3
Luis Sanchez Castellanos <luis.sanchez@schibsted.com>
2018-10-14 00:27:54 +0200

    Test frameworks working

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
+import logo from './logo.svg';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', function() {
+  test('should render an the App without errors and having a logo', function() {
+    expect(shallow(<App />).contains(<img src={logo} className="App-logo" alt="logo" />)).toBe(true);
+  });
 });
